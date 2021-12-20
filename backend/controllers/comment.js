4f4568a16b54ce37bd17d9149b053c122babb515
PaulineCvl <pauline.chevrollier@gmail.com>
2021-12-20 15:58:29 +0100

    error handling comments

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -6,32 +6,32 @@ exports.createComment = (req, res, next) => {
         postId: req.params.id,
         userId: req.userId
     })
-        .then(() => res.status(200).json({ message: 'Commentaire ajouté' }))
-        .catch(error => res.status(500).json({ error }));
+        .then(() => res.status(201).json({ message: 'Commentaire ajouté' }))
+        .catch(error => res.status(400).json({ error }));
 }
 
 exports.getAllComments = (req, res, next) => {
     Comment.findAll({ where: { postId: req.params.id } })
         .then(comments => { res.status(200).json(comments) })
-        .catch(error => res.status(400).json({ error }));
+        .catch(error => res.status(404).json({ error }));
 }
 
 exports.getOneComment = (req, res, next) => {
     Comment.findOne({ where: { id: req.params.id } })
         .then(comment => res.status(200).json(comment))
-        .catch(error => res.status(400).json({ error }));
+        .catch(error => res.status(404).json({ error }));
 }
 
 exports.modifyComment = (req, res, next) => {
     const commentUpdated = req.body;
 
     Comment.update(commentUpdated, { where: { id: req.params.id } })
-        .then(() => res.status(200).json({ message: 'commentaire modifié' }))
+        .then(() => res.status(201).json({ message: 'Commentaire modifié' }))
         .catch(error => res.status(400).json({ error }));
 }
 
 exports.deleteComment = (req, res, next) => {
     Comment.destroy({ where: { id: req.params.id } })
-        .then(() => res.status(200).json({ message: 'commentaire supprimé' }))
+        .then(() => res.status(200).json({ message: 'Commentaire supprimé' }))
         .catch(error => res.status(400).json({ error }));
 }
